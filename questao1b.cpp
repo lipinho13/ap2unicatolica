4159debd1442cf8c05d209a8de442a1a2dc2b3d1
lipinho13 <151794074+lipinho13@users.noreply.github.com>
2025-06-11 19:48:56 -0300

    Add files via upload

--- questao1b.cpp
+++ questao1b.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include <string>
 
-int buscaBinaria(const std::vector<std::string> &livros, const std::string &titulo, int tamanho) {
+int buscaBinaria(const std::vector<std::string> &livros, const std::string &titulo) {
     int inicio = 0;
+    int tamanho = livros.size();
     int fim = tamanho - 1;
 
     while (inicio <= fim) {
@@ -22,23 +23,30 @@ int buscaBinaria(const std::vector<std::string> &livros, const std::string &titu
     return -1;
 }
 
-int main() {
-    int qtde;
-    std::cout << "Quantos livros serão cadastrados? ";
-    std::cin >> qtde;
-    std::cin.ignore();
 
-    std::vector<std::string> listaLivros(qtde);
-    for (int i = 0; i < qtde; ++i) {
-        std::cout << "Título do livro " << i << ": ";
-        std::getline(std::cin, listaLivros[i]);
-    }
+int main() {
+    std::vector<std::string> listaLivros = {
+        "A Culpa é das Estrelas", "A Droga da Obediência", "A Estrada", "A Fera de Gaia", 
+        "A Menina que Roubava Livros", "A Montanha Mágica", "A Revolução dos Bichos", 
+        "A Sombra do Vento", "A Teia de Charlotte", "1984", "Admirável Mundo Novo", 
+        "Alice no País das Maravilhas", "O Alquimista", "Cem Anos de Solidão", 
+        "A Hora da Estrela", "A Metamorfose", "As Crônicas de Nárnia: O Leão, a Feiticeira e o Guarda-Roupa", 
+        "Assassinato no Expresso do Oriente", "Corte de Espinhos e Rosas", "Crepúsculo", 
+        "Dom Casmurro", "Duna", "Ensaio sobre a Cegueira", "Fahrenheit 451", "Frankenstein", 
+        "O Grande Gatsby", "Harry Potter e a Pedra Filosofal", "Jogos Vorazes", 
+        "Memórias Póstumas de Brás Cubas", "O Código Da Vinci", "O Cortiço", 
+        "O Guia do Mochileiro das Galáxias", "O Iluminado", "O Ladrão de Raios", "O Pequeno Príncipe", 
+        "O Poder do Hábito", "O Silmarillion", "O Sol é Para Todos", 
+        "O Senhor dos Anéis: A Sociedade do Anel", "Onde Vivem os Monstros", "Orgulho e Preconceito", 
+        "O Retrato de Dorian Gray", "Percy Jackson e o Ladrão de Raios", "Sapiens: Uma Breve História da Humanidade", 
+        "O Hobbit", "Um Estudo em Vermelho", "Vidas Secas"
+    };
 
     std::string titulo;
-    std::cout << "\nQual título de livro voce procura?: ";
+    std::cout << "\nQual titulo de livro você procura?: ";
     std::getline(std::cin, titulo);
 
-    int posicao = buscaBinaria(listaLivros, titulo, qtde);
+    int posicao = buscaBinaria(listaLivros, titulo);
 
     if (posicao != -1) {
         std::cout << "Livro encontrado na posição vetor[" << posicao << "]" << std::endl;
